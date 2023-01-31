ac20d0f2cc0dc1877f7559be32ea2e588f9085e1
Robin Hanjun Kwon <khj02021@gmail.com>
2023-01-31 16:37:50 +0900

    불필요 파일 삭제 및 그로 인한 App Module 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
@@ -28,7 +26,7 @@ import { typeOrmModuleOptions } from './config/typeorm.config';
     PostsModule,
     CommentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
